fb7cde4780083431bc9879dd41d05b9b4f6a5530
Aljosa Mohorovic <aljosa.mohorovic@gmail.com>
2010-11-04 16:35:05 +0100

    updated jquery, version 1.4.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'django-jquery',
-    version = '0.1.8',
+    version = '0.1.11',
     description = "Django app - bundles jQuery for django-staticfiles",
     long_description = open('README.rst').read(),
     author = 'Aljosa Mohorovic',
