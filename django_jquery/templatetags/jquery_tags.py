42a8750ccf41b9217bf865e85190e532dd034f17
Jeroen Dekkers <jeroen@dekkers.ch>
2022-05-05 21:46:25 +0200

    Support Django 1.11 without any warnings

--- django_jquery/templatetags/jquery_tags.py
+++ django_jquery/templatetags/jquery_tags.py
@@ -1,9 +1,12 @@
 import os
 from django.conf import settings
 from django import template
+from django.utils.html import format_html
 
 register = template.Library()
 
+
 @register.simple_tag
 def include_jquery():
-    return "<script type='text/javascript' src='%s'></script>" % os.path.join(getattr(settings, 'STATIC_URL'), 'jquery', 'jquery-1.7.2.min.js')
+    return format_html("<script type='text/javascript' src='{}'></script>",
+                       os.path.join(getattr(settings, 'STATIC_URL'), 'jquery', 'jquery-1.7.2.min.js'))
